c21260d41340798ca6bb1dd5369134fae91e594c
Thiago Iaia <thiago.iaiapereira@gmail.com>
2021-07-02 16:28:21 -0300

    Adicionados os objetos literais com preenchimento padrão Sicredi e demais campos customizáveis.

---
+++ SicrediHeaderArquivo.mjs
@@ -0,0 +1,32 @@
+export const SicrediHeaderArquivo = {
+    controle: {
+        codigoDoBancoNaCompensacao:     '748',
+        loteDeServico:                  '0000',
+        tipoDeRegistro:                 '0'
+    },
+    usoExclusivoCnabFebraban1:          '         ',
+    empresa: {
+        tipoInscricaoEmpresa:           null,
+        numeroInscricaoEmpresa:         null,
+        codigoConvenioBanco:            '                    ',
+        agenciaMantenedoraConta:        null,
+        digitoVerificadorAgencia:       null,
+        numeroContaCorrente:            null,
+        digitoVerificadorConta:         null,
+        digitoVerificadorAgenciaConta:  ' ',
+        nomeEmpresa:                    null,
+    },
+    nomeBanco:                          'SICREDI                       ',
+    usoExclusivoCnabFebraban2:          '          ',
+    arquivo: {
+        codigoRemessaRetorno:           '1',
+        dataGeracaoArquivo:             null,
+        horarioGeracaoArquivo:          null,
+        numeroSequencialArquivo:        null,
+        numeroVersaoLayoutArquivo:      '081',
+        densidadeGravacaoArquivo:       '01600'
+    },
+    usoReservadoBanco:                  '                    ',
+    usoReservadoEmpresa:                '                    ',
+    usoExclusivoCnabFebraban3:          '                             '
+}
---
+++ SicrediTrailerLote.mjs
@@ -0,0 +1,27 @@
+export const SicrediTrailerLote = {
+    controle: {
+        codigoDoBancoNaCompensacao:     '748',
+        loteDeServico:                  '0001',
+        tipoDeRegistro:                 '5'
+    },
+    usoExclusivoCnabFebraban1:          '         ',
+    quantidadeDeRegistrosNoLote:        null,
+    cobrancaSimples: {
+        quantidade:                     '000000',
+        valor:                          '00000000000000000'
+    },
+    cobrancaVinculada: {
+        quantidade:                     '000000',
+        valor:                          '00000000000000000'
+    },
+    cobrancaCaucionada: {
+        quantidade:                     '000000',
+        valor:                          '00000000000000000'
+    },
+    cobrancaDescontada: {
+        quantidade:                     '000000',
+        valor:                          '00000000000000000'
+    },
+    numeroAvisoLancamento:              '        ',
+    usoExclusivoCnabFebraban2:          '                                                                                                                     '
+}
